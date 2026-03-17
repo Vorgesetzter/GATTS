bf501c9e68d6b10e6cc01498e791eb126e12f566
Oliver Weissl <oliver.weissl@outlook.com>
2026-03-17 15:37:58 +0100

    Refractored

--- Datastructures/enum.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-class AttackMode(Enum):
-    TARGETED = "targeted"
-    NOISE_UNTARGETED = "noise-untargeted"
-    UNTARGETED = "untargeted"
-    ZERO_UNTARGETED = "zero-untargeted"
-    NEGATION_UNTARGETED = "negation-untargeted"
--- Objectives/InterpolationVector/__init__.py
+++
@@ -1,4 +0,0 @@
-from .L1Objective import L1Objective
-from .L2Objective import L2Objective
-
-__all__ = ["L1Objective", "L2Objective"]
